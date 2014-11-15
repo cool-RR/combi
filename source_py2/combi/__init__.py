8ca2afe7c9473f3a31fe267dd86cdfa4d120202d
Ram Rachum <ram@rachum.com>
2014-11-15 16:52:16 +0200

    -

--- source_py2/combi/__init__.py
+++ source_py2/combi/__init__.py
@@ -1,12 +1,25 @@
 # Copyright 2009-2015 Ram Rachum.
 # This program is distributed under the MIT license.
 
+'''
+Combi is a Pythonic package for combinatorics.
+
+Combi lets you explore spaces of permutations and combinations as if they were
+Python sequences, but without generating all the permutations/combinations in
+advance. It lets you specify a lot of special conditions on these spaces. It
+also provides a few more classes that might be useful in combinatorics
+programming.
+
+Visit http://pypi.python.org/pypi/combi/ for more info.
+'''
+
 import combi._python_toolbox
 
 from combi._python_toolbox.math_tools import binomial
 
-from combi._python_toolbox.nifty_collections import (Bag, OrderedBag, FrozenBag,
-                                              FrozenOrderedBag)
+from combi._python_toolbox.nifty_collections import (Bag, OrderedBag,
+                                                     FrozenBag,
+                                                     FrozenOrderedBag)
 
 from .chain_space import ChainSpace
 from .product_space import ProductSpace
